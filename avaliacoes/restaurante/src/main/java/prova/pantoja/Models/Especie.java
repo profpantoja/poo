c10896a66a938c1fa45871057ebafa94cc355091
Nilo <139521840+Nilokrtz@users.noreply.github.com>
2025-06-16 20:53:59 -0300

    Delete avaliacoes/restaurante directory

--- avaliacoes/restaurante/src/main/java/prova/pantoja/Models/Especie.java
+++
@@ -1,9 +0,0 @@
-package prova.pantoja.Models;
-
-public class Especie extends Pagamento {
-
-    @Override
-    public void pagar() {
-        System.out.println("Pagamento realizado em Espécie.");
-    }
-}
--- avaliacoes/restaurante/src/main/java/prova/pantoja/Models/Pagamento.java
+++
@@ -1,7 +0,0 @@
-package prova.pantoja.Models;
-
-public abstract class Pagamento {
-
-    public abstract void pagar();
-
-}
