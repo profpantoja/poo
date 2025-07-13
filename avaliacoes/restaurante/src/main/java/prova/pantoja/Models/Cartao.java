05feae68292556efa2587e6dc139decb078a0b7f
Nilo <nilogabriel21@gmail.com>
2025-07-13 16:01:38 -0300

    Apagando o pacote Restaurante que não tem na main

--- avaliacoes/restaurante/src/main/java/prova/pantoja/Models/Cartao.java
+++
@@ -1,9 +0,0 @@
-package prova.pantoja.Models;
-
-public class Cartao extends Pagamento {
-
-    @Override
-    public void pagar() {
-        System.out.println("Pagamento realizado com Cartão.");
-    }
-}
